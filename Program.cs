b9da0fcd6563528d59a491d9e1f488c5cfb5403b
Emill <asasindark22@gmail.com>
2024-12-16 20:48:18 +0200

    list of strings to save files whose names were not found

--- Program.cs
+++ Program.cs
@@ -10,9 +10,12 @@ namespace Лаба3
     {
         static void Main(string[] args)
         {
-            string[] files = {"10.txt", "11.txt", "12.txt" , "13.txt"
+            string[] files =
+                {"10.txt", "11.txt", "12.txt" , "13.txt"
                  , "14.txt" , "15.txt", "16.txt", "17.txt" , "18.txt",
                 "19.txt", "20.txt", "21.txt","22.txt","23.txt","24.txt" ,"25.txt"};
+            List <string> nofile = new List<string>();
+
         
         
         
