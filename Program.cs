b78ff77f4730535b64ea8d65e6eacf0871d725bf
Emill <asasindark22@gmail.com>
2024-12-16 21:08:48 +0200

    reading files

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -21,7 +22,15 @@ namespace Лаба3
             List<int> result = new List<int>();
             foreach (string file in files)
             {
+                try
+                {
+                    var lines = File.ReadAllLines(file);
 
+
+
+
+
+                }
             }
 
 
