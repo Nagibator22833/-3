c208da32ea64826968c2ff4c92987b9c60c54ee9
Emill <asasindark22@gmail.com>
2024-12-16 20:57:19 +0200

    storing files in which the product goes beyond the 32-bit int

--- Program.cs
+++ Program.cs
@@ -11,11 +11,14 @@ namespace Лаба3
         static void Main(string[] args)
         {
             string[] files =
-                {"10.txt", "11.txt", "12.txt" , "13.txt"
+            { "10.txt", "11.txt", "12.txt" , "13.txt"
                  , "14.txt" , "15.txt", "16.txt", "17.txt" , "18.txt",
-                "19.txt", "20.txt", "21.txt","22.txt","23.txt","24.txt" ,"25.txt"};
+                "19.txt", "20.txt", "21.txt","22.txt","23.txt","24.txt" ,"25.txt"
+            };
             List <string> nofile = new List<string>();
             List<string > badData = new List<string>();
+            List<string>overflow=new List<string>();
+
 
 
         
